7e3c34ef7ded7c51e425911cae68f70ed1f37e49
henrysssilveira <henry.sssilveira@gmail.com>
2024-06-24 16:52:04 -0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("meuFormulario").addEventListener("submit", function(event) {
-    event.preventDefault();  // Impede o envio padrão do formulário
-    window.location.href = "./formulario.html";  // Redireciona para a URL especificada
-});
